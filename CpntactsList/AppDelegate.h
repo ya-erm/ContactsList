caa79b7a03c794e744f875f096874079cd275cf3
Ярослав Ермилов <yass98@kilograpp.com>
2017-02-03 13:48:21 +0300

    Создание проекта XCode

---
+++ CpntactsList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CpntactsList
+//
+//  Created by Ярослав Ермилов on 03.02.17.
+//  Copyright © 2017 Kilograpp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CpntactsList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CpntactsList
+//
+//  Created by Ярослав Ермилов on 03.02.17.
+//  Copyright © 2017 Kilograpp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
